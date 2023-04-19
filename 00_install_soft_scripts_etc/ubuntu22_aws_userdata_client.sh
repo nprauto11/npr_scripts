0e7d619bab3667d6482fcd01b4063aecc0d9adf7
nprauto11 <nprauto11@gmail.com>
2023-04-19 10:25:05 +0530

    updated client script

--- 00_install_soft_scripts_etc/ubuntu22_aws_userdata_client.sh
+++ 00_install_soft_scripts_etc/ubuntu22_aws_userdata_client.sh
@@ -7,16 +7,16 @@ systemctl start nginx
 
 mkdir -p /tmp/utilities/softwares 
 git clone https://github.com/nprauto11/npr_scripts.git /tmp/utilities/softwares
-cd /tmp/utilities/softwares/00_install_soft_scripts
+cd /tmp/utilities/softwares/00_install_soft_scripts_etc
 sh indexpage_nginx_ubuntu22.sh
 
 mkdir -p /tmp/utilities/keys
 git clone https://github.com/nprauto11/npr_ansible_practice.git /tmp/utilities/keys
 cd /tmp/utilities/keys/00_ssh_keys
-cat id_rsa.pub >> /home/${USER}/.ssh/authorized_keys
+cat id_rsa.pub >> ~/.ssh/authorized_keys
 
 cd /tmp && chown -R $user1 utilities
 su - $user1
 cd /tmp/utilities/keys/00_ssh_keys
-cat id_rsa.pub >> /home/${USER}/.ssh/authorized_keys
+cat id_rsa.pub >> ~/.ssh/authorized_keys
 
