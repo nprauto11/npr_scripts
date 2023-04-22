688e1ed1f7b1e078e9b28145ec3d658d1516362a
nprauto11 <nprauto11@gmail.com>
2023-04-22 22:46:00 +0530

    updated init scripts

--- 00_install_soft_scripts_etc/kubernetes_etc/kubeadm_init_master.sh
+++ 00_install_soft_scripts_etc/kubernetes_etc/kubeadm_init_master.sh
@@ -2,9 +2,14 @@
 
 sudo kubeadm init --cri-socket unix:///var/run/cri-dockerd.sock --ignore-preflight-errors=all
 
-sudo mkdir -p $HOME/.kube
+# To start using your cluster, you need to run the following as a regular user:
+mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Alternatively, if you are the root user, you can run:
+# export KUBECONFIG=/etc/kubernetes/admin.conf
+
 
 ## install networking driver -- Weave/flannel/canal/calico etc... 
 ## below installs calico networking driver 
