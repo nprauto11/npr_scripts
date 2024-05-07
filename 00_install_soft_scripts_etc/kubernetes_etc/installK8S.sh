fb3733dac1f2b684f20b05bc58fc0742405b8045
nprauto11 <97073205+nprauto11@users.noreply.github.com>
2024-05-07 19:36:24 +0530

    Update installK8S.sh
    modified install kubernetes script on 07may24

--- 00_install_soft_scripts_etc/kubernetes_etc/installK8S.sh
+++ 00_install_soft_scripts_etc/kubernetes_etc/installK8S.sh
@@ -6,11 +6,16 @@ install_ubuntu() {
     apt-get update
     apt-get install -y apt-transport-https ca-certificates curl
    echo "starting the installation of k8s components (kubeadm,kubelet,kubectl) ...."
-    curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-   echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" |  tee /etc/apt/sources.list.d/kubernetes.list
-    apt-get update
-    apt-get install -y kubelet kubeadm kubectl
+   # curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+   # echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" |  tee /etc/apt/sources.list.d/kubernetes.list
+   # apt-get update
+   # apt-get install -y kubelet kubeadm kubectl
 
+   curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+   echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+   sudo apt update
+   sudo apt install -y kubeadm=1.28.1-1.1 kubelet=1.28.1-1.1 kubectl=1.28.1-1.1
+    
    if [ $? -eq 0 ];then
       echo "kubelet, kubeadm & kubectl are successfully installed"
        apt-mark hold kubelet kubeadm kubectl
